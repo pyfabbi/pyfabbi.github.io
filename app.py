000219e4fd3e8a3de3945aefea6f4707625fa216
pytutorial <51279995+pytutorial@users.noreply.github.com>
2019-08-07 18:02:24 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -5,81 +5,30 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-def readData():
-    f = open('data.json', encoding='utf-8')
+def loadData():
+    f = open('data.json')
     products = json.load(f)
     f.close()
     return products
     
 def saveData(products):
-    f = open('data.json', 'w', encoding='utf-8')
+    f = open('data.json', 'w')
     json.dump(products, f)
     f.close()
     
-def getMaxId(products):
-    if len(products) == 0:
-        return 0
-        
-    return products[-1]['id']
-    
-def findById(products, id):
-    for product in products:
-        if product['id'] == id:
-            return product
-            
-def search(products, keyword):
-    return [p for p in products 
-                if keyword in p['code'] or keyword in p['name']]
-
 @app.route('/products')
 def getProducts():
-    products = readData() 
-    keyword = request.args.get('keyword', '')
-    products = search(products, keyword)
+    products = loadData() 
     return jsonify(products)
-    
-@app.route('/product/<int:id>')
-def getProduct(id):
-    products = readData() 
-    product = findById(products, id)
-    if product:
-        return jsonify(product)
-    
-    return jsonify({'error' : 'Product not found'})
     
 @app.route('/add_product', methods=['POST'])
 def addProduct():
-    products = readData()        
+    products = loadData()        
     product = dict(request.json)
-    product['id'] = 1 + getMaxId(products)    
+    maxid = 0 if len(products) == 0 else products[-1]['id']
+    product['id'] = 1 + maxid
     products.append(product)
     saveData(products)
     return jsonify(product)
-    
-@app.route('/update_product/<int:id>', methods=['POST'])
-def updateProduct(id):
-    products = readData()
-    product = findById(products, id)
-    
-    if product:
-        product.update(request.json)
-        saveData(products)
-        return jsonify(product)
-        
-    return jsonify({'error' : 'Product not found'})
-    
-
-@app.route('/delete_product/<int:id>', methods=['GET'])
-def deleteProduct(id):
-    products = readData()
-    product = findById(products, id)
-    
-    if product:
-        products.remove(product)
-        saveData(products)
-        return jsonify({'success' : True})
-        
-    return jsonify({'success' : False, 'error' : 'Product not found'})
-    
     
 app.run(debug=True)
